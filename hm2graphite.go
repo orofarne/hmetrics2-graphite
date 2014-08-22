e4e7153d7ba45ca9523547caa9d1617f6d52e8a6
Maxim Dementyev <max@orofarne.ru>
2014-08-22 17:42:26 +0400

    hm2graphite.go

---
+++ hm2graphite.go
@@ -0,0 +1,26 @@
+package hmetrics2graphite
+
+import (
+	"fmt"
+	"github.com/marpaia/graphite-golang"
+	"math"
+)
+
+func Exporter(host string, port int) (func(map[string]float64), error) {
+	gr, err = graphite.NewGraphite(host, port)
+	if err != nil {
+		return nil, err
+	}
+	err = gr.Connect()
+	if err != nil {
+		return nil, err
+	}
+	return func(newData map[string]float64) {
+		for k, v := range newData {
+			if !math.IsNaN(v) && !math.IsInf(v, 0) {
+				val, _ := fmt.Sprintf("%v", v)
+				gr.SimpleSend(k, val)
+			}
+		}
+	}
+}
